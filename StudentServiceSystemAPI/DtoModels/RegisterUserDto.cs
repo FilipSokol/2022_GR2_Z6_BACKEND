3495efa0bb36c4392e3068172f6cdf4c1ee9cdd8
Arkadiusz Kędziora <arek.kedziora7@gmail.com>
2022-11-30 18:40:30 +0100

    Nullable dto properties

--- StudentServiceSystemAPI/DtoModels/RegisterUserDto.cs
+++ StudentServiceSystemAPI/DtoModels/RegisterUserDto.cs
@@ -7,8 +7,8 @@
         public string Email { get; set; }
         public string Password { get; set; }
         public string ConfirmPassword { get; set; }
-        public string Nationality { get; set; }
-        public string Province { get; set; }
+        public string? Nationality { get; set; }
+        public string? Province { get; set; }
         public DateTime? DateOfBirth { get; set; }
         public int RoleId { get; set; } = 1;
     }
