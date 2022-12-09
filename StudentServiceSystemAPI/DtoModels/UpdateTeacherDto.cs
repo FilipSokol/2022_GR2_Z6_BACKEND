c80159342301685601ac916cc23fed89375df10f
Arkadiusz Kędziora <arek.kedziora7@gmail.com>
2022-12-09 20:59:53 +0100

    Adjust UpdateTeacherDto

--- StudentServiceSystemAPI/DtoModels/UpdateTeacherDto.cs
+++ StudentServiceSystemAPI/DtoModels/UpdateTeacherDto.cs
@@ -5,8 +5,6 @@
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Email { get; set; }
-        public int SubjectId { get; set; }
-
         public int DepartmentId { get; set; }
     }
 }
